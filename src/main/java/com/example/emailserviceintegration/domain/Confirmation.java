22775768747c4dd1b5b8c1d7be20929d2480e94c
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-10-29 03:51:29 +0100

    working

--- src/main/java/com/example/emailserviceintegration/domain/Confirmation.java
+++ src/main/java/com/example/emailserviceintegration/domain/Confirmation.java
@@ -6,14 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Builder
 @Table(name = "confirmations")
 
 
@@ -22,8 +23,23 @@ public class Confirmation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String token;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
     private LocalDateTime createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
     private User user;
+
+    public Confirmation(User user){
+        this.user =user;
+        this.createdDate = LocalDateTime.now();
+        this.token = UUID.randomUUID().toString();
+    }
+
+
+
 
 
 }
