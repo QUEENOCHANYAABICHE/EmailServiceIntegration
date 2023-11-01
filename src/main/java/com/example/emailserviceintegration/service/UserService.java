77aef00099ec3cd4e76751e93ef4392cb6fdb87d
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-01 18:30:37 +0100

    feat2

--- src/main/java/com/example/emailserviceintegration/service/UserService.java
+++ src/main/java/com/example/emailserviceintegration/service/UserService.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    User saveUser(UserDTO userDTO);
+    //User saveUser(UserDTO userDTO);
     Boolean verifyToken(String token);
 }
